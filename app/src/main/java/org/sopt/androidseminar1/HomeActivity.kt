20477ac4ef1c1dddf89c22e559a1157470b88af4
Daehoww <kdh2769@gmail.com>
2021-10-31 23:33:40 +0900

    [init] #1 level - 1 working

---
+++ app/src/main/java/org/sopt/androidseminar1/HomeActivity.kt
@@ -0,0 +1,56 @@
+package org.sopt.androidseminar1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import com.bumptech.glide.Glide
+import org.sopt.androidseminar1.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+
+        initTransactionEvent()
+        initImage()
+
+        setContentView(binding.root)
+    }
+
+    private fun initImage(){
+        Glide.with(this).load("https://avatars.githubusercontent.com/u/89780201?s=200&v=4")
+            .circleCrop().into(binding.ivProfile)
+
+    }
+
+
+    private fun initTransactionEvent(){
+        val fragment1 = FollowRecyclerView ()
+        val fragment2 = RepositoryRecyclerView()
+
+        supportFragmentManager.beginTransaction().add(R.id.container_main, fragment1).commit()
+
+        binding.btnFollow.setOnClickListener {
+            val transaction = supportFragmentManager.beginTransaction()
+
+            transaction.replace(R.id.container_main, fragment1)
+
+            transaction.commit()
+        }
+
+        binding.btnRepo.setOnClickListener {
+            val transaction = supportFragmentManager.beginTransaction()
+
+            transaction.replace(R.id.container_main, fragment2)
+
+            transaction.commit()
+        }
+
+    }
+
+}
+
